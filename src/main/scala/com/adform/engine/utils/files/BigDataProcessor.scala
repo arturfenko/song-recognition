15332a724fb597cea585567f7dd1d608ab79f21a
arturfenko <a.fenko@gmail.com>
2016-04-26 21:33:27 +0300

    big data processor. minor fix

--- src/main/scala/com/adform/engine/utils/files/BigDataProcessor.scala
+++ src/main/scala/com/adform/engine/utils/files/BigDataProcessor.scala
@@ -7,26 +7,13 @@ import akka.stream.ActorFlowMaterializer
 import akka.stream.io._
 import akka.stream.scaladsl._
 
-/*USAGE
-
-val file = new File("data\\mxm_dataset_train.txt")
-BigDataProcessor.processFile(file, SynchronousFileSource.DefaultChunkSize)
-*/
-
 object BigDataProcessor {
   implicit val system = ActorSystem("Sys")
   implicit val materializer = ActorFlowMaterializer()
 
-  def processFile(file: File, chunkSize: Int): Unit = {
+  def processFile(file: File, chunkSize: Int, process: String => Unit): Unit = {
     val fileSource = SynchronousFileSource(file, chunkSize)
     val flow = fileSource.map(chunk => chunk.utf8String)
-    flow.to(Sink.foreach(processChunk(_))).run
-  }
-
-  //TODO implement chunk processing
-  var counter = 0
-  def processChunk(chunk: String): Unit = {
-    counter = counter + 1
-    println(counter)
+    flow.to(Sink.foreach(process(_))).run
   }
 }
